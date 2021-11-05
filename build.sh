3d0652cfdae2008dd04b597d5e0e85c8b5344824
zbz <nnzbz@21cn.com>
2021-11-05 16:57:02 +0800

    * rac能正常启动

---
+++ build.sh
@@ -0,0 +1,24 @@
+$(cd `dirname $0`; pwd)
+
+cd ../archetype/
+mvn clean install -DskipTests
+cd ../common-pom/
+mvn clean install -DskipTests
+cd ../wheel/
+mvn clean install -DskipTests
+cd ../mbgx/
+mvn clean install -DskipTests
+cd ../robotech/
+mvn clean install -DskipTests
+cd ../common-pom/sb-dependencies
+mvn clean install -DskipTests
+cd ../sbs/
+mvn clean install -DskipTests
+cd ../common-pom/sb-dependencies/sb-parent
+mvn clean install -DskipTests
+cd ../scx/
+mvn clean install -DskipTests
+
+
+
+
