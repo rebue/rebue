cbf430c93cb8c8848b201c9156d2a9e10524ffb6
zbz <nnzbz@21cn.com>
2021-11-11 09:02:03 +0800

    * 注册淘宝jdk

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
-cd $(cd "$(dirname "$0")";pwd)
-cd ../archetype/
+ROOT_DIR=$(cd "$(dirname "$0")/..";pwd)
+cd $ROOT_DIR/archetype/
 mvn clean install -DskipTests
-cd ../common-pom/
+cd $ROOT_DIR/common-pom/
 mvn clean install -DskipTests
-cd ../wheel/
+cd $ROOT_DIR/wheel/
 mvn clean install -DskipTests
-cd ../mbgx/
+cd $ROOT_DIR/mbgx/
 mvn clean install -DskipTests
-cd ../robotech/
+cd $ROOT_DIR/robotech/
 mvn clean install -DskipTests
-cd ../common-pom/sb-dependencies
+cd $ROOT_DIR/common-pom/sb-dependencies
 mvn clean install -DskipTests
-cd ../../sbs/
+cd $ROOT_DIR/sbs/
 mvn clean install -DskipTests
-cd ../common-pom/sb-dependencies/sb-parent
+cd $ROOT_DIR/common-pom/sb-dependencies/sb-parent
 mvn clean install -DskipTests
-cd ../../../scx/
+cd $ROOT_DIR/scx/orp/orp-core/libs/
+mvn install:install-file -Dfile=taobao-sdk-java-auto_1479188381469-20210908.jar -DgroupId=com.dingtalk.open -DartifactId=taobao-sdk-java-auto -Dversion=20210908 -Dpackaging=jar
+cd $ROOT_DIR/scx/
 mvn clean install -DskipTests
